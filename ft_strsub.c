05e569b46aa6e9164eb9d18b73ad8cbf60f06e90
Johanna Josephine <mahthaki@gmail.com>
2019-09-26 16:35:23 -0700

    fixed strsub hopefully

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: jjosephi <jjosephi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 21:37:00 by jjosephi          #+#    #+#             */
-/*   Updated: 2019/09/26 16:31:59 by jjosephi         ###   ########.fr       */
+/*   Updated: 2019/09/26 16:35:08 by jjosephi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,6 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	size_t	i;
 
 	i = 0;
-	if (s == NULL)
-		return (NULL);
-	if (start > ft_strlen(s))
-		return (NULL);
 	if (!(str = malloc(len) + 1))
 		return (NULL);
 	while (i < len && s[start] != '\0')
