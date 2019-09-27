ff5e57c8f0c25a685f5cd643dddb9468badd700a
Johanna Josephine <mahthaki@gmail.com>
2019-09-26 17:07:55 -0700

    fixed strsub hopefully

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: jjosephi <jjosephi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 21:37:00 by jjosephi          #+#    #+#             */
-/*   Updated: 2019/09/26 16:48:04 by jjosephi         ###   ########.fr       */
+/*   Updated: 2019/09/26 17:07:38 by jjosephi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 
-	str = NULL;
 	if (!(ft_strnew(str)))
 		return (NULL);
 	ft_strncpy(str, s + start, len);
