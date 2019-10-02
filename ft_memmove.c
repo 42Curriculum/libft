1be8770af79b0c39e6d31491eed7a91c85e2da58
Johanna Josephine <mahthaki@gmail.com>
2019-10-01 18:33:18 -0700

    memmove is dumb

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jjosephi <jjosephi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 20:52:09 by jjosephi          #+#    #+#             */
-/*   Updated: 2019/10/01 18:31:47 by jjosephi         ###   ########.fr       */
+/*   Updated: 2019/10/01 18:33:10 by jjosephi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,24 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t				i;
+{
+	size_t			i;
+	unsigned char	*nsrc;
+	unsigned char	*ndest;
 
-	i = 0;
-	if ((unsigned char *)src < (unsigned char *)dest)
+	i = -1;
+	ndest = (unsigned char *)dest;
+	nsrc = (unsigned char *)src;
+	if (nsrc > ndest)
 	{
-		while (i <= n)
-		{
-			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-			i++;
-		}
+		while (++i < n)
+			ndest[i] = nsrc[i];
 	}
 	else
 	{
 		i = n;
-		while (i > 0)
-		{
-			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-			i--;
-		}
+		while (--i > 0)
+			ndest[i] = nsrc[i];
 	}
 	return (dest);
 }
