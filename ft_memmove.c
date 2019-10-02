e7c06b2ea9a1d7f65e5f9abca2d5bd745ec68db3
Johanna Josephine <mahthaki@gmail.com>
2019-10-01 18:34:06 -0700

    memmove is dumb

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,14 +6,13 @@
 /*   By: jjosephi <jjosephi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 20:52:09 by jjosephi          #+#    #+#             */
-/*   Updated: 2019/10/01 18:33:10 by jjosephi         ###   ########.fr       */
+/*   Updated: 2019/10/01 18:34:00 by jjosephi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
-{
 {
 	size_t			i;
 	unsigned char	*nsrc;
